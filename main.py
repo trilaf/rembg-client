b2ee6bba6881066417fe668cb682ed9648fc3382
Faris <trilukman41@gmail.com>
2024-11-02 11:42:14 +0700

    fix output path location for Windows

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from rembg import remove;
-import sys;
+from rembg import remove
+import sys
+import os
 
-def set_output_path(input_path):
-	filename_without_format = input_path.split("/")[-1].split(".")[0]
-	output_filename = filename_without_format + ".png"
-	return "/".join(input_path.split("/")[:-1]) + f"/{output_filename}"
+def path_separator() -> str:
+  return "\\" if os.name == "nt" else "/"
+
+def set_output_path(input_path) -> str:
+  filename_without_format = input_path.split(path_separator())[-1].split(".")[0]
+  output_filename = path_separator() + filename_without_format + ".png"
+  output_path = ""
+  if os.name != "nt":
+    output_path += "/"
+  return output_path + (path_separator()).join(input_path.split(path_separator())[:-1]) + output_filename
 
 def rembg_main(input_path = ""):
   if len(input_path) == 0:
@@ -12,17 +19,21 @@ def rembg_main(input_path = ""):
     return
   output_path = set_output_path(input_path)
   try:
-  	with open(input_path, 'rb') as i:
-  		with open(output_path, 'wb') as o:
-  			print(f"Processing: {input_path}")
-  			input = i.read()
-  			output = remove(input, post_process_mask=True)
-  			o.write(output)
-  			print(f"\nImage output location: {output_path}")
-  			return
+    with open(input_path, 'rb') as i:
+      try:
+        with open(output_path, 'wb') as o:
+          print(f"Processing: {input_path}")
+          input = i.read()
+          output = remove(input, post_process_mask=True)
+          o.write(output)
+          print(f"\nImage output location: {output_path}")
+          return
+      except FileNotFoundError:
+        print("Error: Output path maybe invalid")
+        return
   except FileNotFoundError:
-  	print(f"Error: Input file not exist")
-  	return
+    print(f"Error: Input file not exist")
+    return
 
 if __name__ == "__main__":
   args = sys.argv[1:]
